1806a56911c3866b5e775b2eea9f8510788ead56
Joel Edwards <joeledwards@gmail.com>
2023-06-19 01:18:33 -0600

    feat: slack signature validation and tests

--- test/persistence/key.test.js
+++ test/persistence/key.test.js
@@ -1,7 +1,7 @@
 const tap = require('tap')
 const { makeConversationKey } =  require('../../lib/persistence/key')
 
-tap.test('makeConversationKey should return null given an invalid input event', assert => {
+tap.test('makeConversationKey should return null given an invalid input event', async assert => {
   assert.equal(makeConversationKey(), undefined)
   assert.equal(makeConversationKey(null), undefined)
   assert.equal(makeConversationKey({}), undefined)
@@ -16,7 +16,7 @@ tap.test('makeConversationKey should return null given an invalid input event', 
   assert.equal(makeConversationKey({ evnet: {            channel: '1', ts: 0        } }), undefined)
 })
 
-tap.test('makeConversationKey should create a valid conversation key given a valid event', assert => {
+tap.test('makeConversationKey should create a valid conversation key given a valid event', async assert => {
   assert.equal(makeConversationKey({ event: { team: 'A', channel: '1', thread_ts: 0, ts: 1 } }), 'conversation/team=A/channel=1/thread_ts=0')
   assert.equal(makeConversationKey({ event: { team: 'A', channel: '1', thread_ts: 0 } }), 'conversation/team=A/channel=1/thread_ts=0')
 })
---
+++ test/slack/auth.test.js
@@ -0,0 +1,77 @@
+const tap = require('tap')
+
+const { validateSignature } = require('../../lib/slack/auth')
+
+tap.test('validateSignature invalid if signing key is missing', async assert => {
+  assert.same(validateSignature({
+    context: {
+    },
+    headers: {
+      'x-slack-signature': 'deadbeef',
+      'x-slack-request-timestamp': '100',
+    },
+    body: Buffer.from('{"message":"test"}'),
+  }), { valid: false, reasons: ['No signing key in context'] })
+})
+
+tap.test('validateSignature invalid if slack signature is missing', async assert => {
+  assert.same(validateSignature({
+    context: {
+      slackSigningSecret: 'signing-key',
+    },
+    headers: {
+      'x-slack-request-timestamp': '100',
+    },
+    body: Buffer.from('{"message":"test"}'),
+  }), { valid: false, reasons: ['No slack signature header'] })
+})
+
+tap.test('validateSignature invalid if slack timestamp is missing', async assert => {
+  assert.same(validateSignature({
+    context: {
+      slackSigningSecret: 'signing-key',
+    },
+    headers: {
+      'x-slack-signature': 'deadbeef',
+    },
+    body: Buffer.from('{"message":"test"}'),
+  }), { valid: false, reasons: ['No slack timestamp header'] })
+})
+
+tap.test('validateSignature invalid if there is no body', async assert => {
+  assert.same(validateSignature({
+    context: {
+      slackSigningSecret: 'signing-key',
+    },
+    headers: {
+      'x-slack-signature': 'deadbeef',
+      'x-slack-request-timestamp': '100',
+    },
+  }), { valid: false, reasons: ['No body'] })
+})
+
+tap.test('validateSignature invalid if signature does not match', async assert => {
+  assert.same(validateSignature({
+    context: {
+      slackSigningSecret: 'signing-key',
+    },
+    headers: {
+      'x-slack-signature': 'v0=57570c6717f7ca57f9053dbee135abc66fe507b23ac9a173c7a70db1ef7e94dd',
+      'x-slack-request-timestamp': '100',
+    },
+    body: Buffer.from('{"message":"unexpected"}'),
+  }), { valid: false, reasons: ['Signature does not match'] })
+})
+
+tap.test('validateSignature valid if signature matches', async assert => {
+  assert.same(validateSignature({
+    context: {
+      slackSigningSecret: 'signing-key',
+    },
+    headers: {
+      'x-slack-signature': 'v0=57570c6717f7ca57f9053dbee135abc66fe507b23ac9a173c7a70db1ef7e94dd',
+      'x-slack-request-timestamp': '100',
+    },
+    body: Buffer.from('{"message":"expected"}'),
+  }), { valid: true })
+})

8971bb68900653470408373bb76c490e04744c67
Joel Edwards <joeledwards@gmail.com>
2023-06-22 23:56:15 -0600

    feat: added handler test (error with aws init)

--- test/handler.js
+++
@@ -1,5 +0,0 @@
-const tap = require('tap')
-const slackEventHandler = require('../lib/handler')
-
-tap.test('handler.slackHandler should return a 401 for requests with an invalid signature', async assert => {
-})
---
+++ test/handler.test.js
@@ -0,0 +1,84 @@
+const tap = require('tap')
+
+const { getContext } = require('../lib/context')
+const slackEventHandler = require('../lib/handler').makeHandler()
+
+function baseSlackEvent () {
+  return {
+    headers: {
+      'x-slack-signature': 'deadbeef',
+      'x-slack-request-timestamp': '0',
+    },
+    body: JSON.stringify({
+      type: 'event_callback',
+      event: {
+        type: 'app_mention',
+        text: 'Are you there?',
+      },
+    }),
+  }
+}
+
+tap.test('slackEventHandler should return a 201 for a valid request', async assert => {
+  const context = await getContext({ isTest: true })
+  const slackData = {}
+
+  const future = {}
+  future.promise = new Promise(resolve => {
+    future.resolve = resolve
+  })
+
+  context.services.slack = {
+    sendResponse: async ({ context, event, message }) => {
+      slackData.context = context
+      slackData.event = event
+      slackData.message = message
+      future.resolve()
+    }
+  }
+
+  const event = baseSlackEvent()
+
+  assert.same(
+    await slackEventHandler({ context, event }),
+    { status: 201, data: { message: 'Prompt received' } }
+  )
+
+  await future.promise
+
+  assert.same(slackData.message, 'howdy')
+})
+
+tap.test('slackEventHandler should retain context across multiple conversations', async assert => {
+  const context = await getContext({ isTest: true })
+  const event = baseSlackEvent()
+  const modelData = {}
+
+  const future = {}
+  future.promise = new Promise(resolve => {
+    future.resolve = resolve
+  })
+
+  context.services.model = {
+    engageModel: async ({ messages }) => {
+      modelData.messages = messages
+    }
+  }
+
+  context.services.slack = {
+    sendResponse: async ({ context, event, message }) => {
+      future.resolve()
+    }
+  }
+
+  await slackEventHandler({ context, event })
+  await slackEventHandler({ context, event })
+  await future.promise
+
+  assert.same(modelData.messages, [
+    {role: 'user', content: 'Are you there?' },
+    {role: 'assistant', content: 'hi there' },
+    {role: 'user', content: 'Are you there?' },
+    {role: 'assistant', content: 'hi there' },
+  ])
+})
